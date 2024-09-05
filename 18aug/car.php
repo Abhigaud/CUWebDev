7a2a45d7f3dce9e2adfb2b3b6468b4fde0dddbba
CUWebDev <abhishekgaud.annu@gmail.com>
2024-09-05 07:52:43 +0530

    car

---
+++ 18aug/car.php
@@ -0,0 +1,17 @@
+<?php
+// to have a car in my Application
+// Should be able to print the details of each cars
+
+$firstCarName = "ferrari";
+$colorFirstCar = "white";
+
+$secondCarName ="BMW";
+$coloreSecondCar = "rew";
+
+echo "first Car Name : $firstCarName";
+echo "Second Car Name : $secondCarName";
+
+echo "first Car Colour : $colorFirstCar";
+echo "Second Car Colour : $colorSecondCar";
+?>
+
